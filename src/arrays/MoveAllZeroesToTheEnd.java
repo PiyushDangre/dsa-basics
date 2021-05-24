da0ac33a9e8eb72e5f92d68f3c14b0d114b884c7
Piyush Dangre <piyushdangre11@gmail.com>
2021-05-24 19:12:52 +0530

    Simple program to move all zeroes in an array to the end

---
+++ src/arrays/MoveAllZeroesToTheEnd.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MoveAllZeroesToTheEnd {
+
+	/**
+	 * Count variable here is just the count of non zero elements
+	 * 
+	 * @param arr
+	 */
+	public static void moveAllZeroesToEnd(int[] arr) {
+		int arrLength = arr.length;
+		int count = 0;
+		for (int i = 0; i < arrLength; i++) {
+			if (arr[i] != 0) {
+				swap(count, i, arr);
+				count++;
+			}
+		}
+	}
+
+	public static void swap(int a, int b, int arr[]) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+
+	public static void main(String[] args) {
+		int[] arr1 = { 10, 20, 0, 0, 0, 20, 0, 40, 90, 0 };
+		System.out.println("Before removal - " + Arrays.toString(arr1));
+		moveAllZeroesToEnd(arr1);
+		System.out.println("After removal - " + Arrays.toString(arr1));
+
+	}
+}

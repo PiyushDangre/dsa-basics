ed18f86363aeb2904c31e4335de50ebef388c4d4
Piyush Dangre <piyushdangre11@gmail.com>
2021-06-01 00:22:27 +0530

    Added Moore's voting algorithm to solve majority element problem.

--- src/arrays/MajorityElement.java
+++ src/arrays/MajorityElement.java
@@ -38,8 +38,60 @@ public class MajorityElement {
 		return res;
 	}
 	
+	/**
+	 * - This solution uses Moore's Voting algorithm.
+	 * - Better explanation can be found at following YouTube video
+	 * - https://www.youtube.com/watch?v=n5QY3x_GNDg
+	 * 
+	 * - In this algorithm , what we are doing basically is cancelling the non matching elements by matching elements.
+	 * - Doing this in the end givesus the remaining majority element.
+	 * - We maintain two pointers - majorityElement and count
+	 * - We traverse from left and assume count to be one and the first element as majority element.
+	 * - Then for each matching element we increment count by 1.
+	 * - if element is non matching, then decrement count byb 1.
+	 * - if count becomes 0 then reset values of majorityElement and count.
+	 * 
+	 * - In the later part of this solution we need to find whether the element is actual majority or not by comparing with n/2.
+	 * @param arr
+	 * @return
+	 */
+	public static int majorityElement(int arr[]) {
+		int n = arr.length ;
+		int count = 1 ;
+		int majorityElement = arr[0];
+		
+		for(int i=1; i<n;i++) {
+			if(arr[i] == majorityElement) {
+				count++;
+			}else {
+				count-- ;
+			}
+			
+			if(count == 0) {
+				count = 1;
+				majorityElement = arr[i];
+			}
+		}
+		
+		// 2nd part of solu[tion only to verify if the element is actual majority
+		int actualCount = 0 ;
+		for(int i=0; i<n; i++) {
+			if(arr[i] == majorityElement) {
+				actualCount++;
+			}
+		}
+		
+		// if element not majority, then return -1
+		if(actualCount <= n/2) {
+			majorityElement = -1; 
+		}
+		
+		return majorityElement;
+	}
+	
 	public static void main(String args[]) {
 		int arr[] = {8,7,6,8,6,6,6};
 		System.out.println("Majority element by naive method is "+naiveSolution(arr));
+		System.out.println("Majority element using Moore's voting algorithm is "+ majorityElement(arr));
 	}
 }
